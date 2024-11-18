62a99866a3c1337cc3f4c7f2bb7d0c3dfd12c7b5
Villyvu <villyvu@github.com>
2024-11-18 21:18:55 +0100

    Roslyn LSP
    Roslyn LSP now works

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -184,7 +184,7 @@ return {
 
         html = {},
 
-        csharp_ls = {},
+        -- csharp_ls = {},
 
         lua_ls = {
 
@@ -226,21 +226,61 @@ return {
             require('lspconfig')[server_name].setup(server)
           end,
         },
-        ['csharp_ls'] = function()
-          local lsp = require 'lspconfig'
-          lsp.csharp_ls.setup {
-            config = {
-              handlers = {
-                ['textDocument/definition'] = require('csharpls_extended').handler,
-                ['textDocument/typeDefinition'] = require('csharpls_extended').handler,
-              },
-              cmd = { csharpls },
-              -- rest of your settings
-            },
-          }
-        end,
-        require('telescope').load_extension 'csharpls_definition',
+        -- ['csharp_ls'] = function()
+        --   local lsp = require 'lspconfig'
+        --   lsp.csharp_ls.setup {
+        --     config = {
+        --       handlers = {
+        --         ['textDocument/definition'] = require('csharpls_extended').handler,
+        --         ['textDocument/typeDefinition'] = require('csharpls_extended').handler,
+        --       },
+        --       cmd = { csharpls },
+        --       -- rest of your settings
+        --     },
+        --   }
+        -- end,
+        -- require('telescope').load_extension 'csharpls_definition',
       }
     end,
+  },
+  {
+    -- C# Roslyn LSP Configuration
+    'seblj/roslyn.nvim',
+    ft = 'cs', -- Set filetype for C#
+    opts = {
+      -- The configuration specific to roslyn.nvim
+      config = {
+        settings = {
+          -- C# background analysis settings
+          ['csharp|background_analysis'] = {
+            background_analysis_dotnet_analyzer_diagnostics_scope = 'fullSolution',
+            background_analysis_dotnet_compiler_diagnostics_scope = 'fullSolution',
+          },
+          -- C# completion settings
+          ['csharp|completion'] = {
+            dotnet_show_completion_items_from_unimported_namespaces = true,
+          },
+          -- C# inlay hints settings
+          ['csharp|inlay_hints'] = {
+            csharp_enable_inlay_hints_for_implicit_object_creation = true,
+            csharp_enable_inlay_hints_for_implicit_variable_types = true,
+            csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+            csharp_enable_inlay_hints_for_types = true,
+            dotnet_enable_inlay_hints_for_indexer_parameters = true,
+            dotnet_enable_inlay_hints_for_literal_parameters = true,
+            dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+            dotnet_enable_inlay_hints_for_other_parameters = true,
+            dotnet_enable_inlay_hints_for_parameters = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+            dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+          },
+          -- C# code lens settings
+          ['csharp|code_lens'] = {
+            dotnet_enable_references_code_lens = true,
+          },
+        },
+      },
+    },
   },
 }
