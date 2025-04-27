b36615c0079c59960da7003f181717745230d272
Yugi <villy_vu983@live.dk>
2025-04-27 20:07:08 +0200

    added new plugins

--- init.lua
+++ init.lua
@@ -599,6 +599,8 @@ require('lazy').setup({
   require 'kickstart.plugins.autoclose',
   require 'kickstart.plugins.dadbod',
   require 'kickstart.plugins.easydotnet',
+  require 'kickstart.plugins.surround',
+  require 'kickstart.plugins.treesj',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
